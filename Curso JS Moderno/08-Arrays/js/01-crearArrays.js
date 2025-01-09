5d3d48a0ac53be0fc2554b1045017f860dbd11eb
Melanie Vite <melanie.vite@imss.gob.mx>
2025-01-09 17:01:45 -0600

    Acceder a arreglos

--- Curso JS Moderno/08-Arrays/js/01-crearArrays.js
+++ Curso JS Moderno/08-Arrays/js/01-crearArrays.js
@@ -1,8 +1,21 @@
 //Crear Arrays
-//Los arreglos sirven para agrupar elementos del MISMO TIPO 
+//Los arreglos sirven para agrupar elementos que se recomienda sean del MISMO TIPO 
 const numeros = [10, 20, 30];
 
 //Segunda forma de crear arreglos no tan usada 
 const meses = new Array('Enero', 'Febrero', 'Marzo');
 
 console.log(numeros);
+console.log(meses);
+
+//Se pueden crear arreglos con un solo valor 
+const carrito = [ 10 ];
+
+//Un arreglo que contiene datos de todo tipo
+const deTodo = ['Hola', 10, true, 'si', null, { nombre: 'Melanio', trabajo: 'Programador'}];
+
+console.log(deTodo);
+
+//JS no nos va a marcar error si tenemos un arreglo con distintos tipos de datos
+//Se trata de una variable que contenga multiples elementos
+//Se puede tener un arreglo dentro de otros arreglos
---
+++ Curso JS Moderno/08-Arrays/js/02-accederArray.js
@@ -0,0 +1,6 @@
+//Para acceder a los elementos dentro del array
+const num = [ 10, 20, 30, 40, 50];
+
+console.log(num);
+//Visualizar arreglos con tabla 
+console.table(num);
