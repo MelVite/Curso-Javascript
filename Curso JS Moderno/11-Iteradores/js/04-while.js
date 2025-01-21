1ac45e0991c9518c1ac7677e9e0e1bf4e73865fb
Melanie Vite <melanie.vite@imss.gob.mx>
2025-01-21 17:00:26 -0600

    Trabajanding ...

---
+++ Curso JS Moderno/11-Iteradores/js/04-while.js
@@ -0,0 +1,14 @@
+//While se ejecuta mientras una condición sea verdadera 
+/**Consta de 3 partes
+ * 
+ */
+
+let i = 0;
+
+while(i < 10) { //Condición
+
+    console.log(`Número ${i}`);
+
+    i++; //Incremento
+
+}
