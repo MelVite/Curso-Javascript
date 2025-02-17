6fc49442039edf1857a41ce8ceb9469875b63527
Melanie Vite <melanie.vite@imss.gob.mx>
2025-02-17 17:02:56 -0600

    Tenemos proyecto

---
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    //Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+}); //Esto se va a ejecutar una vez todo el html esta cargado
